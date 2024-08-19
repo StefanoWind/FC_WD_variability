e6d81817216f3ae943a4f9de0268a04f0304d8f5
StefanoWind <stefano.letizia@nrel.gov>
2024-08-19 11:22:02 -0600

    added interp check

--- Plot_all_maps.py
+++ Plot_all_maps.py
@@ -28,20 +28,17 @@ matplotlib.rcParams['mathtext.fontset'] = 'cm'
 matplotlib.rcParams['font.size'] = 14
 
 #%% Inputs
-source=os.path.join(cd,'data/DS*nc')
+source=os.path.join(cd,'data/45deg/DS*nc')
 source_nwtc='data/NWTC.xlsx'
-source_ws='data/wspdDifference_wdirBins_M5 61m_M2 50m_20230401_20240508.csv'
 
 #graphics 
 xlim=[-250,1500]#[m]
 ylim=[-875,875]#[m]
-plot_rows=3
-plot_cols=6
+plot_rows=2
+plot_cols=4
 
 #%% Initalization
 FC=pd.read_excel(source_nwtc).set_index('Site')
-
-WS=pd.read_csv(source_ws)
 
 #locations
 xy=utm.from_latlon(FC['Lat'].values,FC['Lon'].values)
@@ -75,7 +72,7 @@ y_solar=y_solar/N
 
 files=glob.glob(source)
 fig=plt.figure(figsize=(18,10))
-gs = gridspec.GridSpec(plot_rows, plot_cols, width_ratios=[1]*(plot_cols-1)+[0.1])
+gs = gridspec.GridSpec(plot_rows, plot_cols+1, width_ratios=[1]*plot_cols+[0.1])
 
 wd_all=[]
 DS_selected_all=[]
@@ -92,23 +89,29 @@ for f in files:
     X,Y=np.meshgrid(Data.x[sel_x],Data.y[sel_y])
     
     points=np.array([X.ravel(),Y.ravel()]).T
-    values=Data.DS.values[sel_y,:][:,sel_x].ravel()
-    DS_selected=griddata(points,values,([FC['x']['M2'],FC['x']['M5'],x_solar],[FC['y']['M2'],FC['y']['M5'],y_solar]))
+    values=Data.DS.values[sel_x,:][:,sel_y].T.ravel()
+    DS_sel2=griddata(points,values,([FC['x']['M2'],FC['x']['M5'],x_solar],[FC['y']['M2'],FC['y']['M5'],y_solar]))
+    
+    DS_sel=[np.float64(Data.DS.interp({'x':FC['x']['M2'],'y':FC['y']['M2']}).values),
+            np.float(Data.DS.interp({'x':FC['x']['M5'],'y':FC['y']['M5']}).values),
+            np.float(Data.DS.interp({'x':x_solar,'y':y_solar}).values)]
+    
+    print(str(wd)+': '+str(np.round(DS_sel,2)))
+    print(str(wd)+': '+str(np.round(DS_sel2,2)))
     
     ax = plt.subplot(gs[int(ctr/plot_cols), ctr-int(ctr/plot_cols)*plot_cols])
    
-    cf=plt.contourf(Data.x,Data.y,Data.DS,np.arange(-30,31,2.5),vmin=-30,vmax=30,cmap='seismic',extend='both')
+    cf=plt.contourf(Data.x,Data.y,Data.DS.T,np.arange(-30,31,2.5),vmin=-30,vmax=30,cmap='seismic',extend='both')
     
     plt.plot(FC['x']['M2'],FC['y']['M2'],'^g',markersize=15)
-    plt.text(FC['x']['M2']-150,FC['y']['M2']-250,str(np.round(DS_selected[0],1))+' %',bbox=dict(facecolor='white', alpha=0.5))
+    plt.text(FC['x']['M2']-150,FC['y']['M2']-250,str(np.round(DS_sel[0],1))+' %',bbox=dict(facecolor='white', alpha=0.5))
     
     plt.plot(FC['x']['M5'],FC['y']['M5'],'^g',markersize=15)
-    plt.text(FC['x']['M5']-150,FC['y']['M5']-250,str(np.round(DS_selected[1],1))+' %',bbox=dict(facecolor='white', alpha=0.5))
-    
+    plt.text(FC['x']['M5']-150,FC['y']['M5']-250,str(np.round(DS_sel[1],1))+' %',bbox=dict(facecolor='white', alpha=0.5))
    
     polygon = Polygon(vertices, closed=True, edgecolor='g',facecolor=(0,1,0,0.5))
     ax.add_patch(polygon)     
-    plt.text(x_solar-150,y_solar-250,str(np.round(DS_selected[2],1))+' %',bbox=dict(facecolor='white', alpha=0.5))
+    plt.text(x_solar-150,y_solar-250,str(np.round(DS_sel[2],1))+' %',bbox=dict(facecolor='white', alpha=0.5))
     
     plt.plot(x_turbine,y_turbine,'ow',markersize=15,linewidth=5)
     plt.plot(x_turbine,y_turbine,'3k',markersize=15)
@@ -124,24 +127,11 @@ for f in files:
     ax.arrow(1250,625,utl.cosd(270-wd)*150,utl.sind(270-wd)*150,head_width=300, head_length=100, fc='g', ec='k',width=200)
     utl.axis_equal()
     ctr+=1
-    
-    DS_selected_all=utl.vstack(DS_selected_all,DS_selected)
-    wd_all=np.append(wd_all,wd)
 
 utl.remove_labels(fig)
 
 cbar_ax = plt.subplot(gs[:, -1])
 cbar = fig.colorbar(cf, cax=cbar_ax,label=r'$\Delta S$ [%]')
 
-#%% Plots
-matplotlib.rcParams['font.size'] = 22
-plt.figure(figsize=(18,6))
-plt.plot(WS['Wind direction (deg)'],WS['Wind speed difference (m/s)'],'.-b',markersize=10,label='Data')
-plt.plot(wd_all,(DS_selected_all[:,0]/100-DS_selected_all[:,1]/100)/(DS_selected_all[:,1]/100+1)*100,'.-k',markersize=10,label='JH75 model')
-plt.legend()
-plt.xlabel(r'$\theta_w$ [$^\circ$]')
-plt.ylabel(r'$\frac{\overline{u}_{M2}(z=h+l)-\overline{u}_{M5}(z=h+l)}{\overline{u}_{M5}(z=h+l)}$ [%]')
-plt.xticks(wd_all)
-plt.grid()
-plt.tight_layout()
+
 
